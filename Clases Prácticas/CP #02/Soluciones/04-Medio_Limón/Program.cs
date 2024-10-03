3ee6626aa006a4d752f21fd7077349b8a11999f8
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 19:31:25 -0400

    Añadiendo: Solución del problema 4 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/04-Medio_Limón/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce una cadena de caracteres: ");
+    string? texto = Console.ReadLine();
+    Console.WriteLine(texto + " medio limón");
+  }
+}
