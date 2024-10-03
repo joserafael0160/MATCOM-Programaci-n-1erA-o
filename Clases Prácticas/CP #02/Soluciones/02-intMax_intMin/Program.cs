52bb42e0ee6468204443a61493b061c99222164e
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 19:17:52 -0400

    Añadiendo: Solución del problema 2 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/02-intMax_intMin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    int intMax = int.MaxValue;
+    int intMin = int.MinValue;
+
+    Console.WriteLine("El valor máximo admitido por el tipo int es: " + intMax + "\nEl valor mínimo admitido por el tipo int es: " + intMin);
+  }
+}
