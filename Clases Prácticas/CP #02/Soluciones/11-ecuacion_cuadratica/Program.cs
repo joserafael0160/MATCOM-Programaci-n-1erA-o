d833a6cd03339b04cf16b82b899c69803b6bac60
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-04 17:18:37 -0400

    Añadiendo: Solución del problema 11 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/11-ecuacion_cuadratica/Program.cs
@@ -0,0 +1,22 @@
+﻿// Fórmula de Bhaskara
+// (-b +- sqrt((b * b) - (4 * a * c)))/(2 * a)
+
+using System;
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introduce b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introduce c: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    
+    int divisor = 2 * a;
+    int calc = (b * b) - (4 * a * c);
+    double x1 = (-b + Math.Sqrt(calc)) / divisor;
+    double x2 = (-b - Math.Sqrt(calc)) / divisor;
+    Console.WriteLine($"Soluciones: {x1}, {x2}");
+  }
+}
