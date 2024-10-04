44fbfb3696db5578b3d5ec9c561acfe1815bc64c
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 21:22:35 -0400

    Añadiendo: Solución del problema 7 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/07-promedio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce un número: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introduce otro número: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    float promedio = (a + b)/2f;
+    Console.WriteLine("El promedio de esos números es: " + promedio);
+  }
+}
