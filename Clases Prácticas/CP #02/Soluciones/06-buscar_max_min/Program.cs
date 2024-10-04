d85001f615e2f37d41be2c8c5f45ae95af278962
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 21:02:14 -0400

    Añadiendo: Solución del problema 6 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/06-buscar_max_min/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+  static int Max(int a, int b)
+  {
+      return (a + b + Math.Abs(a - b)) / 2;
+  }
+
+  static int Min(int a, int b)
+  {
+    return (a + b - Math.Abs(a - b)) / 2;
+  }
+
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce un número: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introduce otro número: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int maxValue = Max(a,b);
+    int minValue = Min(a,b);
+    Console.WriteLine("El mayor es: " + maxValue + "\nY el menor es: " + minValue);
+
+  }
+}
