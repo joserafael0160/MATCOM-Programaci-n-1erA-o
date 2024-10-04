77d3948aa74f3db81a1f5a90c4f13d154ae6c2fc
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 20:04:06 -0400

    Añadiendo: Solución del problema 5 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/05-doble/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce un número entero: ");
+    int dnum = Convert.ToInt32(Console.ReadLine()) * 2;
+    Console.WriteLine("El doble del número es: " + dnum);
+  }
+}
