c9d5aebacfb30b1cf88803dcfe84daab32bfa20f
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-04 12:36:31 -0400

    Añadiendo: Solución del problema 9 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/09-intercambio_variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+  
+  static void Main(string[] args)
+  {
+    int a = 5;
+    int b = 9;
+    /*
+      1-  Con variable auxiliar
+          int c = a;
+          a = b;
+          b = c;
+      
+      2-  a = a + b
+          b = b - a
+          a = a - b 
+    */
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    Console.WriteLine($"El valor de a es {a} y el valor de b es {b}");
+
+  }
+}
