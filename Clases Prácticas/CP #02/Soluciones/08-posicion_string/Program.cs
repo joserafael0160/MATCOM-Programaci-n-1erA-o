ae4626d170e45b2e9199826e2ae1352b09d55c0c
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 22:01:35 -0400

    Añadiendo: Solución del problema 8 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/08-posicion_string/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce una cadena de caracteres: ");
+    string? texto = Console.ReadLine();
+    Console.WriteLine("Introduce un número entero menor que: " + texto?.Length);
+    int numInd = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("El carácter que está en la posicion " + numInd + " es " + texto?[numInd - 1]);
+  }
+}
