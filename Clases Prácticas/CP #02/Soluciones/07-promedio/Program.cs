a0ac5259caa044c1c326d3e7d86167a483137ed9
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-03 21:43:53 -0400

    Corrigiendo: Ejercicio 7

--- Clases Prácticas/CP #02/Soluciones/07-promedio/Program.cs
+++ Clases Prácticas/CP #02/Soluciones/07-promedio/Program.cs
@@ -8,7 +8,10 @@ class Program
     int a = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Introduce otro número: ");
     int b = Convert.ToInt32(Console.ReadLine());
-    float promedio = (a + b)/2f;
-    Console.WriteLine("El promedio de esos números es: " + promedio);
+    Console.WriteLine("Introduce otro número: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int medio = a + b + c - Math.Min(a, Math.Min(b, c)) - Math.Max(a, Math.Max(b, c));
+    float promedio = (a + b + c)/3f;
+    Console.WriteLine("El número medio es " + medio + "\nY el promedio de esos números es: " + promedio);
   }
 }
