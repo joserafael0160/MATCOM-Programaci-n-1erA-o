e1b8bdd7606ac8e6cffb9f360045d53c255889fa
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-04 18:00:24 -0400

    Añadiendo: Solución del problema 12 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/12-velocidad_escritura/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce una oración");
+    int startT = Environment.TickCount;
+    string? sentence = Console.ReadLine();
+    int finishT = Environment.TickCount;
+    int result = finishT - startT;
+    Console.WriteLine($"Para escribir la oración: {sentence} \nTe demoraste {result / 60} segundos");
+  }
+}
