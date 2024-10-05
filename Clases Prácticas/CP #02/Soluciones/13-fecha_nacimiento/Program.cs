7f2afd564a41380624f63f455a62a74887123321
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-05 00:00:10 -0400

    Añadiendo: Solución del problema 13 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/13-fecha_nacimiento/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce el número de tu carnet de identidad: ");
+    long ID = Convert.ToInt64(Console.ReadLine());
+    int aux = Convert.ToInt32(ID / 100000); 
+    int day =  aux % 100;
+    aux = aux / 100;
+    int month = aux % 100;
+    aux = aux / 100;
+    int year = aux;
+    Console.WriteLine($"{day}/{month}/{year}");
+    
+  }
+}
