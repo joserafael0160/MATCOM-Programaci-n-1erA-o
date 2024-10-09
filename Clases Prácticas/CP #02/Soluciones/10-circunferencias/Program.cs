160903193aa6b21fd9ef298eba7814b743f6c4df
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-09 13:36:45 -0400

    Añadiendo: Solución del problema 10 de la CP #02

---
+++ Clases Prácticas/CP #02/Soluciones/10-circunferencias/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args) 
+  {
+    Console.WriteLine("Introduce el radio: ");
+    double r = Convert.ToDouble(Console.ReadLine());
+    double AngleArea = (Math.PI *(r * r))/ 6;
+    double triangle = (Math.Sqrt(3) * (r * r)) / 4;
+    double ang = AngleArea - triangle;
+    double area = (triangle * 2) + (ang * 4);
+    // area = (((Math.Sqrt(3) * (r * r)) / 4) * 2) + ((((Math.PI *(r * r))/ 6) - ((Math.Sqrt(3) * (r * r)) / 4)) * 4);
+    Console.WriteLine($"El área es: {area}cm^2");
+  }
+}
