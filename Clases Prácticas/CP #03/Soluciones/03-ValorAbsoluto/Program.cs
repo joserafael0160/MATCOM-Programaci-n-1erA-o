8d3a7e3481fe56b6e7a0714363efe94d8aad16b8
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-17 15:34:47 -0400

    Añadiendo: Solución del problema 3 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/03-ValorAbsoluto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class AbsoluteValue
+{
+  static void Main(string[] args)
+  {
+    Console.Write("Introduce un valor: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int absValue = x < 0 ? x * -1 : x;
+    Console.WriteLine($"\nEl valor absoluto de {x} es: {absValue}");
+  }
+}
