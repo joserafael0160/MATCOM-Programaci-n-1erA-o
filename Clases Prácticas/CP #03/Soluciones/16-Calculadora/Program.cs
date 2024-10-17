455e69f291caae58d26c2d2e67badd80ef4b4b22
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-17 16:38:20 -0400

    Añadiendo: Solución del problema 16 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/16-Calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Calculator
+{
+  static void Main(string[] args) 
+  {
+    Console.Write("Introduzca un número: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Introduzca otro número: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Introduzca la operación que desea hacer (+, -, *, /): ");
+    string? option = Console.ReadLine();
+
+    Console.Write("\n");
+
+    switch (option)
+    {
+      case "+":
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+      break;
+      
+      case "-":
+        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+      break;
+      
+      case "*":
+        Console.WriteLine($"{num1} x {num2} = {num1 * num2}");
+      break;
+
+      case "/":
+        if (num2 == 0)
+        {
+          Console.WriteLine("No es posible dividir entre 0");
+          Environment.Exit(1);
+        }
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+      break;
+
+      default:
+        Console.WriteLine("Ha introducido un operador invalido");
+      break;
+    }
+  }
+}
