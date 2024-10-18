054ee86664047c2d9193a13c8ca2dc424f57ac27
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-18 12:58:32 -0400

    Añadiendo: Solución del problema 15 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/15-Mayor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Max
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Introduce un número");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introduce otro número");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introduce otro número");
+    int num3 = Convert.ToInt32(Console.ReadLine());
+    
+    int max1 = num1 >= num2 ? num1 : num2;
+    int max = max1 >= num3 ? max1 : num3;
+
+    Console.WriteLine($"\nDe {num1}, {num2} y {num3} el mayor es: {max}.");
+  }
+}
