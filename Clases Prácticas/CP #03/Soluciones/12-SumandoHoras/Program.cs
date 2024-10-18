76ce2aee222c52d425513ee7378b9751dc943fd5
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-18 14:25:32 -0400

    Añadiendo: Solución del problema 12 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/12-SumandoHoras/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class AddingHours
+{
+  static void Main()
+  {
+    Console.Write("Diga la hora a sumar: ");
+    int? addHour = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Diga los minutos a sumar: ");
+    int? addMin = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Diga la hora inicial: ");
+    string?[]? initialTime = Console.ReadLine()?.Split(":");
+
+    int? addTimeMin = (addHour * 60) + Convert.ToInt32(addMin);
+    int initialTimeMin = (Convert.ToInt32(initialTime?[0]) * 60) + Convert.ToInt32(initialTime?[1]);
+  
+    int? totalMinutes = addTimeMin + initialTimeMin;
+    int? finalHours = totalMinutes / 60;
+    int? finalMin = totalMinutes % 60;
+    Console.WriteLine($"\nEl tiempo es de: {finalHours}:{finalMin}");
+  }
+}
