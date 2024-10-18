04c43e9942f22afedcb230b0344725bc8e5a5265
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-18 13:40:05 -0400

    Añadiendo: Solución del problema 13 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/13-Avion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Plane
+{
+  static void Main()
+  {
+    Console.Write("Diga la hora de salida del avión: ");
+    string?[]? departure = Console.ReadLine()?.Split(":");
+    Console.Write("Diga la hora de llegada del avión: ");
+    string?[]? arrival = Console.ReadLine()?.Split(":");
+   
+    int departureMin = Convert.ToInt32(departure?[0]) * 60 + Convert.ToInt32(departure?[1]);
+    int arrivalMin = Convert.ToInt32(arrival?[0]) * 60 + Convert.ToInt32(arrival?[1]);
+
+    int dif =  arrivalMin - departureMin;
+    int hour = dif / 60;
+    int minutes = dif % 60;
+    
+    Console.WriteLine($"\nEl tiempo de vuelo fue de {hour}:{minutes}");
+  }
+}
