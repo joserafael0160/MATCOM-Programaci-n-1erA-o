ad17f27aa4329e96e8392a410477a5e641ebabe9
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-22 15:07:10 -0400

    Añadiendo: Solución del problema 10 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/10-ImprimiendoNumeros/Program.cs
@@ -0,0 +1,23 @@
+﻿class PrintingDescNumbers
+{
+  static void Main(string[] args)
+  {
+    Console.Write("Introduce un número: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"\nLos números en orden descendente de {num} hasta 0 son: ");
+    PrintDescNum(num);
+  }
+
+  static int PrintDescNum(int num)
+  {
+    if (num < 0)
+    {
+      Console.WriteLine();
+      return num;
+    }
+
+    Console.Write(num + " ");
+    return PrintDescNum(num - 1);
+  }
+}
