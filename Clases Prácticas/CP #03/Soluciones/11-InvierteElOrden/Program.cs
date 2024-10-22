fbe336aed2f3edace554950f351af26db15198b5
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-22 15:19:23 -0400

    Añadiendo: Solución del problema 11 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/11-InvierteElOrden/Program.cs
@@ -0,0 +1,23 @@
+﻿class PrintingAscNumbers
+{
+  static void Main(string[] args)
+  {
+    Console.Write("Introduce un número: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"\nLos números en orden ascendente de 0 hasta {num} son: ");
+    PrintAscNum(num);
+  }
+
+  static int PrintAscNum(int finalNumber, int num1 = 0)
+  {
+    if (finalNumber < num1)
+    {
+      Console.WriteLine();
+      return 1;
+    }
+
+    Console.Write(num1 + " ");
+    return PrintAscNum(finalNumber, num1 + 1);
+  }
+}
