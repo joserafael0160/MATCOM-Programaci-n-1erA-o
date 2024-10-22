30ad2aca8d4f432d15bebeb9d6385899447ea259
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-10-22 15:32:23 -0400

    Añadiendo: Solución del problema 9 de la CP #03

---
+++ Clases Prácticas/CP #03/Soluciones/09-Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿class FactorialProgram
+{
+  static void Main()
+  {
+    Console.Write("Introduce un número: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    int factorial = Factorial(num);
+
+    Console.WriteLine($"\nEl factorial de {num} es: {factorial}");
+
+  }
+
+  static int Factorial(int num)
+  {
+    if (num <= 1)
+    {
+      return 1;
+    }
+    return Factorial(num - 1) * num;
+  }
+}
