c223ae5af04e308f0593243e47090765924151f4
José Rafael <120344544+joserafael0160@users.noreply.github.com>
2024-10-24 17:50:55 -0400

    Actualizando: Ejercicio 1 de la CP #02

--- Clases Prácticas/CP #02/Soluciones/01-Hello World!/Program.cs
+++ Clases Prácticas/CP #02/Soluciones/01-Hello World!/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/**************************************************************
+Nombre: José Rafael Pérez Rivero (C112)
+
+Ejercicio:
+  Muestre en la consola el siguiente string: ”Hello, World!”.
+**************************************************************
+Complejidad Big O: O(1)
+**************************************************************/
+
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
