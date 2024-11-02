ad2e7bf1f3cc740b4f0b9f03783087e80f939efc
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-11-02 18:57:30 -0400

    Añadiendo: Solución del problema 2 de la CP #04

---
+++ Clases Prácticas/CP #04/Soluciones/02-SumaDeImpares/Program.cs
@@ -0,0 +1,31 @@
+﻿/**************************************************************
+Nombre: José Rafael Pérez Rivero (C112)
+
+Ejercicio:
+    Implemente un programa que reciba un entero n e imprima 
+    la suma de los primeros n números impares.
+**************************************************************
+Complejidad: O(n)
+**************************************************************/
+
+using System;
+
+class OddSum
+{
+  static int CalcOddSum(int n)
+  {
+    int sum = 0;
+    for (int i = 0, odd = 1; i < n; i++, odd += 2) sum += odd;
+
+    return sum;
+  }
+  static void Main(string[] args)
+  {
+    Console.Write("Introduzca un número: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int oddSum = CalcOddSum(number);
+    
+    Console.WriteLine($"\nLa suma de los primeros {number} números impares es: {oddSum}");
+  }
+}
