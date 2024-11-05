2acfb6f3279ac49fb8db68ee72d82ed4521084c3
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-11-05 18:36:21 -0500

    Añadiendo: Solución del problema 16 de la CP #04

---
+++ Clases Prácticas/CP #04/Soluciones/16-EsPalindromo/Program.cs
@@ -0,0 +1,39 @@
+﻿/**************************************************************
+Nombre: José Rafael Pérez Rivero (C112)
+
+Ejercicio:
+    Implemente un método que determine si s es 
+    palíndromo (se lee igual al derecho que al
+    revés). 
+    Ejemplos: ana, anitalavalatina, zz.
+**************************************************************
+Complejidad: O(n)
+**************************************************************/
+
+using System;
+
+class Program
+{
+  static bool IsPalindrome(string a)
+  {
+    int middle = a.Length;
+    int lastPosition = a.Length - 1;
+    for (int i = 0; i < middle; i++, lastPosition--)
+    {
+      if (a[i] != a[lastPosition]) return false;
+    }
+    return true;
+  }
+  
+  static string Answer(string a) => IsPalindrome(a) ? $"✅ Sí es un palíndromo - {a}" : $"❌ No es un palíndromo - {a}";
+
+  static void Main(string[] args)
+  {
+    string[] testCases = ["Hola", "ana", "zz", "anitalavalatina", "er", "a"];
+
+    foreach (string testCase in testCases)
+    {
+      Console.WriteLine(Answer(testCase));
+    }
+  }
+}
