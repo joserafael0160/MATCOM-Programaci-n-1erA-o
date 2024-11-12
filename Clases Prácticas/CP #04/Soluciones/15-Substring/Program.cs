b2ef9c1de24e9804f65f033ceb2c287e2d5eb251
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-11-12 18:08:35 -0500

    Añadiendo: Solución del problema 15 de la CP #04

---
+++ Clases Prácticas/CP #04/Soluciones/15-Substring/Program.cs
@@ -0,0 +1,59 @@
+﻿/**************************************************************
+Nombre: José Rafael Pérez Rivero (C112)
+
+Ejercicio:
+    Un substring (subcadena) es una secuencia de caracteres 
+    que aparece consecutivamente en una cadena mayor. Dados 
+    los string s y x, implemente un método que diga si x es 
+    substring de s. 
+    Ejemplo: 
+    1 - ”” es substring de toda cadena 
+    2 - ”a” es substring de ”casa” 
+    3 - ”asap” no es substring de ”casa”.
+**************************************************************
+Complejidad: O(n)
+**************************************************************/
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    (string, string)[] testCases = {
+      ("casa",""),
+      ("casa","house"),
+      ("asac","casa"),
+      ("asac","aa"),
+      ("asac","aaa"),
+      ("a","b"),
+      ("a","bbab")
+    };
+
+    foreach ((string firstWord, string secondWord) in testCases)
+    {
+      Console.WriteLine(Answer(firstWord, secondWord));
+    }
+  }
+  static string Answer(string a, string b) => IsSubstring(a, b) ? $"✅ Sí, {b} es substring de {a}" : $"❌ No, {b} no es substring de {a}";
+  public static bool IsSubstring(string firstWord, string secondWord)
+  {
+    bool[] seen = new bool[firstWord.Length];
+    foreach (char characterOfSecondWord in secondWord)
+    {
+      bool flag = false;
+      for (int i = 0; i < firstWord.Length; i++)
+      {
+        if (characterOfSecondWord == firstWord[i] && !seen[i])
+        {
+          flag = true;
+          seen[i] = true;
+          break;
+        } 
+      }
+      
+      if (!flag) return false;
+    }
+    return true;
+  }
+}
